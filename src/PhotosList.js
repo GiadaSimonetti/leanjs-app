915b3178b5c9cdcad6da014a9cd0af5b1795866d
Agata Golebiewska <agata.golebiewska@telegraph.co.uk>
2018-06-19 18:58:06 +0100

    Create PhotosList component

---
+++ src/PhotosList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PhotosList extends Component {
+    render() {
+        return (
+            <ul>
+                These are our photos!
+            </ul>
+        )
+    }
+}
+
+
+export default PhotosList;
