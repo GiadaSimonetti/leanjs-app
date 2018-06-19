85343b6dec2a63980606d6053ff4b6c99159b9f9
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-06-19 19:52:42 +0100

    Photo component

--- src/PhotosList.js
+++ src/PhotosList.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 class PhotosList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      photos: [],
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/photos?_limit=30')
+      .then(photos => {
+        return photos.json();
+      }).then(data => {
+        let pictures = data.photos.map((photo) => {
+          return (
+            <div key={photo.photos}>
+              <img src={photo.thumbnailUrl} />
+            </div>
+          )
+        })
+        this.setState({pictures: pictures});
+        console.log("state", this.state.pictures);
+      })
+
+  }
+
     render() {
         return (
-            <ul>
-                These are our photos!
-            </ul>
+          <div>
+            {this.state.pictures}
+          </div>
         )
     }
 }
