445f6e929a3db18863782e6eab9c1448c6df1c2e
Agata Golebiewska <agata.golebiewska@telegraph.co.uk>
2018-06-19 20:08:27 +0100

    App renders all the photos

--- src/PhotosList.js
+++ src/PhotosList.js
@@ -1,39 +1,40 @@
 import React, { Component } from 'react';
 
 class PhotosList extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      photos: [],
-    };
-  }
-
-  componentDidMount(){
-    fetch('http://jsonplaceholder.typicode.com/photos?_limit=30')
-      .then(photos => {
-        return photos.json();
-      }).then(data => {
-        let pictures = data.photos.map((photo) => {
-          return (
-            <div key={photo.photos}>
-              <img src={photo.thumbnailUrl} />
-            </div>
-          )
-        })
-        this.setState({pictures: pictures});
-        console.log("state", this.state.pictures);
-      })
-
-  }
-
-    render() {
-        return (
-          <div>
-            {this.state.pictures}
-          </div>
-        )
+    constructor(){
+        super();
+        this.state = {
+        photos: [],
+        };
     }
-}
+
+    componentDidMount(){
+        fetch('http://jsonplaceholder.typicode.com/photos?_limit=30')
+        .then(photos => {
+            return photos.json();
+        }).then(data => {
+            let pictures = data.map((photo) => {
+            return (
+                <div key={photo.id}>
+                    <img src={photo.thumbnailUrl} />
+                </div>
+            )
+            })
+            this.setState({pictures: pictures});
+            console.log('data', data);
+            // console.log("state", this.state.pictures);
+        })
+
+    }
+
+        render() {
+            return (
+            <div>
+                {this.state.pictures}
+            </div>
+            )
+        }
+    }
 
 
-export default PhotosList;
+    export default PhotosList;
